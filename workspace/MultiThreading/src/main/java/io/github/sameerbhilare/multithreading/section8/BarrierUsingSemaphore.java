30824e4bf7fd3cc39fec688d0209c3c99431d344
Sameer Bhilare <bhilare.sameer@gmail.com>
2026-02-14 19:23:21 +0530

    Barrier implementation using Semaphores

---
+++ workspace/MultiThreading/src/main/java/io/github/sameerbhilare/multithreading/section8/BarrierUsingSemaphore.java
@@ -0,0 +1,89 @@
+package io.github.sameerbhilare.multithreading.section8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BarrierUsingSemaphore {
+
+    public static void main(String [] args) throws InterruptedException {
+        int numberOfThreads = 5; //or any number you'd like
+
+        List<Thread> threads = new ArrayList<>();
+
+        Barrier barrier = new Barrier(numberOfThreads);
+        for (int i = 0; i < numberOfThreads; i++) {
+            threads.add(new Thread(new CoordinatedWorkRunner(barrier)));
+        }
+
+        for(Thread thread: threads) {
+            thread.start();
+        }
+    }
+
+    public static class Barrier {
+        /*
+        We initialize the semaphore to 0, to make sure every thread that tries to acquire the semaphore gets blocked.
+        And the last thread to get to the barrier, releases the semaphore numberOfWorkers - 1
+        since "numberOfWorkers - 1"  threads are blocked on the semaphore.
+         */
+        private final int numberOfWorkers;
+        private final Semaphore semaphore = new Semaphore(0);
+        private int counter = 0;
+        private final Lock lock = new ReentrantLock();
+
+        public Barrier(int numberOfWorkers) {
+            this.numberOfWorkers = numberOfWorkers;
+        }
+
+        public void waitForOthers() throws InterruptedException {
+            lock.lock();
+            boolean isLastWorker = false;
+            try {
+                counter++;
+
+                if (counter == numberOfWorkers) {
+                    isLastWorker = true;
+                }
+            } finally {
+                lock.unlock();
+            }
+
+            if (isLastWorker) {
+                semaphore.release(numberOfWorkers-1); // last thread to reach the barrier releases all the other threads that are waiting on the semaphore.
+            } else {
+                semaphore.acquire(); // if numberOfThreads = 5, 4 threads will be blocked here, and the last thread to reach the barrier will release all of them.
+            }
+        }
+    }
+
+    public static class CoordinatedWorkRunner implements Runnable {
+        private final Barrier barrier;
+
+        public CoordinatedWorkRunner(Barrier barrier) {
+            this.barrier = barrier;
+        }
+
+        @Override
+        public void run() {
+            try {
+                task();
+            } catch (InterruptedException e) {
+            }
+        }
+
+        private void task() throws InterruptedException {
+            // Performing Part 1
+            System.out.println(Thread.currentThread().getName()
+                    + " part 1 of the work is finished");
+
+            barrier.waitForOthers();
+
+            // Performing Part2
+            System.out.println(Thread.currentThread().getName()
+                    + " part 2 of the work is finished");
+        }
+    }
+}
